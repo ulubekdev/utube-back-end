9cca68901e3739ec5c74c515d6765f26d7337df8
Ulug'bek <93920465+theulubek@users.noreply.github.com>
2022-04-22 16:18:05 +0500

    change databasepath

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 app.use(fileUpload());
 app.use(cors());
 app.use(express.json());
-app.use(modelMiddleware({ databasePath: path.join(process.cwd(), 'database')}));
+app.use(modelMiddleware({ databasePath: path.join(process.cwd(), 'database', 'users') }));
 
 // routes:
 app.use(usersRouter);
