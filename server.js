05ea6a3a01250aeecbdb5c79804ff8e3698d9d07
Ulug'bek <93920465+theulubek@users.noreply.github.com>
2022-04-24 22:49:18 +0500

    changed something

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ import modelMiddleware from './middlewares/model.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: 'http://localhost:4000' }));
+app.use(cors());
 
 app.get('/info', (req, res) => res.send('Hello World!'));
 
